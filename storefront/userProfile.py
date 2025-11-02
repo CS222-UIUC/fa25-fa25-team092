3f32640d4cd692e11f7bbb6c3bce6ab63a5d56a8
dnmeerman <dnmeerman@gmail.com>
2025-11-02 13:21:28 -0600

    removing mongo based schema

--- storefront/userProfile.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import Document, StringField, IdField, ListField, connect
-
-# Connect to MongoDB
-# connect() # once database is created
-
-class User(Document):
-    username = StringField(required=True, max_length=50, unique=True)
-    netId = IdField(required=True, unique=True)
-    likedSongs = ListField(StringField(max_length=30)) # List of song IDs, Song would be another schema
