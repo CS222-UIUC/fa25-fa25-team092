06fc6673ffd81c1b8c9a82fc19cd6fc3f4b3a7c4
dnmeerman <dnmeerman@gmail.com>
2025-12-13 12:41:49 -0600

    merged into rest of project

--- backend/school_project/urls.py
+++ backend/school_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('register/', views.register),
